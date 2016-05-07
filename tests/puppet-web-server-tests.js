832bfa4002c59ce4bf203d32060e366e11471a1e
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-05-08 04:31:26 +0800

    start with message...

--- tests/puppet-web-server-tests.js
+++ tests/puppet-web-server-tests.js
@@ -40,39 +40,87 @@ test('Server smoking tests', function (t) {
   server.init()
   .then(() => {
     t.ok(true, 'server:' + PORT + ' inited')
-    
-    const options = require('url').parse(`https://localhost:${PORT}/ding`)
-    options.rejectUnauthorized = false // permit self-signed CA
 
-    https.get(options, (res) => {
-      t.pass('https server inited')
+    Promise.all([
+      dingHttps()     // retHttps
+      , dingSocket()  // retSocket
+      , dingBrowser() // retBrowser
+      , dingProxy()   // retProxy
+      , getLoginStatusCode() // retStatus
+    ]).then(([
+      retHttps
+      , retSocket
+      , retBrowser
+      , retProxy
+      , retStatus
+    ]) => {
+      t.equal(retHttps ,  'dong', 'ding https   got dong')
+      t.equal(retSocket,  'dong', 'ding socket  got dong')
+      t.equal(retBrowser, 'dong', 'ding browser got dong')
+      t.equal(retProxy,   'dong', 'ding proxy   got dong')
 
-      res.on('data', chunk => {
-        t.equal(chunk.toString(), 'dong', 'https get /ding return dong')
-      })
-    }).on('error', e => {
-      t.fail('https get error:' + e)
+      t.equal(typeof retStatus, 'number', 'status is number')
+
+      t.end() + server.quit()
+    }).catch(e => {
+      t.fail('ding promise rejected:' + e)
+      t.end() + server.quit()
     })
 
-    function testDing() {
-      if (!server.socketClient) {
-        console.error('waiting socketClient to connect for 500ms...')
-        setTimeout(testDing, 500)
-        return
-      }
-      server.socketClient.on('dong', (data) => {
-        t.equal(data, 'dong', 'socket io sent ding got dong')
+    function dingHttps() {
+      const options = require('url').parse(`https://localhost:${PORT}/ding`)
+      options.rejectUnauthorized = false // permit self-signed CA
 
-        server.quit()
-        t.end()
+      return new Promise((resolve, reject) => {
+        https.get(options, res => {
+          res.on('data', chunk => {
+            console.log('https on data got: ' + chunk.toString())
+            resolve(chunk.toString())
+          })
+        }).on('error', e => reject('https get error:' + e))
       })
-      server.socketClient.emit('ding')
     }
-    testDing()
+
+    function dingSocket() {
+      const maxTime   = 3000
+      const waitTime  = 500
+      let   totalTime = 0
+      return new Promise((resolve, reject) => {
+        setTimeout(testDing, waitTime)
+
+        function testDing() {
+          if (!server.socketClient) {
+            totalTime += waitTime
+            if (totalTime > maxTime) 
+              return reject('timeout after ' + totalTime + 'ms');
+
+            console.error('waiting socketClient to connect for ' + totalTime + '/' + maxTime + ' ms...')
+            setTimeout(testDing, waitTime)
+            return
+          }
+          server.socketClient.on  ('dong', data => {
+            console.log('socket on dong got: ' + data)
+            resolve(data)
+          })
+          server.socketClient.emit('ding')
+        }
+      })
+    }
+
+    function dingBrowser() {
+      return server.browserExecute('return Wechaty.ding()')
+    }
+
+    function dingProxy() {
+      return server.proxyWechaty('ding')
+    }
+
+    function getLoginStatusCode() {
+      return server.Wechaty_getLoginStatusCode()
+    }
 
   }).catch((e) => {
     t.fail('server init promise rejected:' + e)
   })
-  console.error('here')
 })
 
