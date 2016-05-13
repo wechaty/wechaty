049701058da323209fb75b07928608b514623cba
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-05-14 01:50:36 +0800

    use co/yield to sync test

--- tests/puppet-web-server-tests.js
+++ tests/puppet-web-server-tests.js
@@ -1,6 +1,7 @@
 const https = require('https')
 const log   = require('npmlog')
 const test  = require('tape')
+const co    = require('co')
 const Server = require('../src/puppet-web-server')
 
 test('Server basic tests', function(t) {
@@ -37,96 +38,87 @@ test('Server smoke testing', function(t) {
   const PORT = 58788
   const server = new Server({port: PORT})
 
-  server.init()
-  .then(() => {
+  co(function* () {
+    yield server.init()
     t.ok(true, 'server:' + PORT + ' inited')
 
-    Promise.all([
-      dingHttps()     // retHttps
-      , dingSocket()  // retSocket
-      , dingBrowser() // retBrowser
-      , dingProxy()   // retProxy
-      , getLoginStatusCode() // retStatus
-    ]).then(([
-      retHttps
-      , retSocket
-      , retBrowser
-      , retProxy
-      , retStatus
-    ]) => {
-      t.equal(retHttps ,  'dong', 'ding https   got dong')
-      t.equal(retSocket,  'dong', 'ding socket  got dong')
-      t.equal(retBrowser, 'dong', 'ding browser got dong')
-      t.equal(retProxy,   'dong', 'ding proxy   got dong')
+    const retHttps = yield dingHttps()
+    t.equal(retHttps ,  'dong', 'ding https   got dong')
 
-      // XXX sometimes object? 201605
-      t.equal(typeof retStatus, 'number', 'status is number')
+    const retSocket = yield dingSocket()
+    t.equal(retSocket,  'dong', 'ding socket  got dong')
 
-      t.end() + server.quit()
-    }).catch(e => {
-      t.fail('ding promise rejected:' + e)
-      t.end() + server.quit()
-    })
+    const retBrowser = yield dingBrowser()
+    t.equal(retBrowser, 'dong', 'ding browser got dong')
 
-    function dingHttps() {
-      const options = require('url').parse(`https://localhost:${PORT}/ding`)
-      options.rejectUnauthorized = false // permit self-signed CA
+    const retProxy = yield dingProxy()
+    t.equal(retProxy,   'dong', 'ding proxy   got dong')
 
-      return new Promise((resolve, reject) => {
-        https.get(options, res => {
-          res.on('data', chunk => {
-            log.verbose('TestingServer', 'https on data got: ' + chunk.toString())
-            resolve(chunk.toString())
-          })
-        }).on('error', e => reject('https get error:' + e))
-      })
-    }
-
-    function dingSocket() {
-      const maxTime   = 3000
-      const waitTime  = 500
-      let   totalTime = 0
-      return new Promise((resolve, reject) => {
-        setTimeout(testDing, waitTime)
-
-        function testDing() {
-          log.silly('TestingServer', server.socketio)
-          if (!server.socketClient) {
-            totalTime += waitTime
-            if (totalTime > maxTime) {
-              return reject('timeout after ' + totalTime + 'ms')
-            }
-
-            log.verbose('TestingServer', 'waiting socketClient to connect for ' + totalTime + '/' + maxTime + ' ms...')
-            setTimeout(testDing, waitTime)
-            return
-          }
-          log.silly('TestingServer', server.socketClient)
-          server.socketClient.on('dong', data => {
-            log.verbose('TestingServer', 'socket on dong got: ' + data)
-            resolve(data)
-          })
-          server.socketClient.emit('ding')
-          server.socketClient.emit('ding')
-          server.socketClient.emit('ding')
-        }
-      })
-    }
-
-    function dingBrowser() {
-      return server.browserExecute('return Wechaty.ding()')
-    }
-
-    function dingProxy() {
-      return server.proxyWechaty('ding')
-    }
-
-    function getLoginStatusCode() {
-      return server.proxyWechaty('getLoginStatusCode')
-    }
-
-  }).catch((e) => {
+    const retStatus = yield getLoginStatusCode()
+    // XXX sometimes object? 201605
+    t.equal(typeof retStatus, 'number', 'status is number')
+  }).catch(e => {
     t.fail('server init promise rejected:' + e)
   })
+
+  server.quit()
+  return t.end()
+
+  function dingHttps() {
+    const options = require('url').parse(`https://localhost:${PORT}/ding`)
+    options.rejectUnauthorized = false // permit self-signed CA
+
+    return new Promise((resolve, reject) => {
+      https.get(options, res => {
+        res.on('data', chunk => {
+          log.verbose('TestingServer', 'https on data got: ' + chunk.toString())
+          resolve(chunk.toString())
+        })
+      }).on('error', e => reject('https get error:' + e))
+    })
+  }
+
+  function dingSocket() {
+    const maxTime   = 3000
+    const waitTime  = 500
+    let   totalTime = 0
+    return new Promise((resolve, reject) => {
+      setTimeout(testDing, waitTime)
+
+      function testDing() {
+        log.silly('TestingServer', server.socketio)
+        if (!server.socketClient) {
+          totalTime += waitTime
+          if (totalTime > maxTime) {
+            return reject('timeout after ' + totalTime + 'ms')
+          }
+
+          log.verbose('TestingServer', 'waiting socketClient to connect for ' + totalTime + '/' + maxTime + ' ms...')
+          setTimeout(testDing, waitTime)
+          return
+        }
+        log.silly('TestingServer', server.socketClient)
+        server.socketClient.on('dong', data => {
+          log.verbose('TestingServer', 'socket on dong got: ' + data)
+          resolve(data)
+        })
+        server.socketClient.emit('ding')
+        server.socketClient.emit('ding')
+        server.socketClient.emit('ding')
+      }
+    })
+  }
+
+  function dingBrowser() {
+    return server.browserExecute('return Wechaty.ding()')
+  }
+
+  function dingProxy() {
+    return server.proxyWechaty('ding')
+  }
+
+  function getLoginStatusCode() {
+    return server.proxyWechaty('getLoginStatusCode')
+  }
 })
 
