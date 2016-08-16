64f1fc686c828d2be5941f4d2657fdd0e88dab26
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-16 21:04:12 +0800

    modulized IoBot

--- bin/io-bot.js
+++ bin/io-bot.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const Wechaty = require('..')
+const log   = require('../src/npmlog-env')
+const IoBot = require('../src/io-bot')
 
-const log = Wechaty.log
 
 const welcome = `
 | __        __        _           _
@@ -35,45 +35,20 @@ if (!token) {
   log.info('Bot', 'Starting for WECHATY_TOKEN: %s', token)
 }
 
-const bot = new Wechaty({
+const ioBot = new IoBot({
   profile
-  , token // token for wechaty.io auth
+  , token
+  , log
 })
 
-bot
-.on('login'	  , user => log.info('Bot', `${user.name()} logined`))
-.on('logout'	, user => log.info('Bot', `${user.name()} logouted`))
-.on('scan'    , ({url, code}) => log.info('Bot', '[%d] %s <-- Scan QR Code in url to login', code, url))
-.on('message', m => {
-  m.ready()
-    .then(onMessage)
-    .catch(e => log.error('Bot', 'ready: %s' , e))
-})
-
-bot.init()
+ioBot.init()
 .catch(e => {
   log.error('Bot', 'init() fail: %s', e)
   bot.quit()
   process.exit(-1)
 })
 
-const onMessage = function(m) {
-  const from = m.from()
-  const to = m.to()
-  const content = m.toString()
-  const room = m.room()
 
-  // log.info('Bot', '%s<%s>:%s'
-  //               , (room ? '['+room.name()+']' : '')
-  //               , from.name()
-  //               , m.toStringDigest()
-  //         )
-
-  if (/^wechaty|botie/i.test(m.get('content')) && !bot.self(m)) {
-    bot.reply(m, 'https://www.wechaty.io')
-      .then(_ => log.info('Bot', 'REPLIED to magic word "wechaty"'))
-  }
-}
 /**
  *
  * To make heroku happy
