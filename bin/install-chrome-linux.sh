0ae4ebf1b96d210af17a749cb024fb43c4e5f042
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-21 13:40:44 +0800

    script for remember how to install chrome in linux

---
+++ bin/install-chrome-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+google-chrome --version
+
+sudo apt-get install -y xvfb
+
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+
+
