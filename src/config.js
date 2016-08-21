8a99836b7ab132b832ec3abc9fc2549623f6d0a6
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-21 12:30:57 +0800

    move config default values to package.json

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
-const Config = {
-  DEFAULT_HEAD:           'chrome'
-  , DEFAULT_PUPPET:       'web'
-  , DEFAULT_PORT:         8788  // W(87) X(88), ascii char code ;-]
-  , DEFAULT_WEB_PORT:  58788
-}
+const Config = require('../package.json').wechaty
 
 module.exports = Config
