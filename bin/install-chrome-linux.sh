f0c47521589a53bfbcf61cd4b7d9df99e8be2a74
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-21 13:42:35 +0800

    kiss

--- bin/install-chrome-linux.sh
+++ bin/install-chrome-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
 google-chrome --version
