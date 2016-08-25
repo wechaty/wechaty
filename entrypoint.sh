6eb9f398b6c7a00edfb69aea89eca05ea846f7d9
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-25 13:07:23 +0000

    fix test syntax from bash to sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,21 @@ if [ "$WECHATY_HEAD" != "phantomjs" ]; then
   Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 fi
 
-if [ "$1" == "start" ]; then
+if [ "$1" = "start" ]; then
     exec npm start
     exit $?
 fi
 
-if [ "$1" == "test" ]; then
+if [ "$1" = "test" ]; then
     exec npm test
     exit $?
 fi
 
-if [ "$1" == "shell" ] || \
-   [ "$1" == "sh" ] || \
-   [ "$1" == "bash" ] || \
-   [ "$1" == "/bin/bash" ] || \
-   [ "$1" == "/bin/sh" ];
+if [ "$1" = "shell" ] || \
+   [ "$1" = "sh" ] || \
+   [ "$1" = "bash" ] || \
+   [ "$1" = "/bin/bash" ] || \
+   [ "$1" = "/bin/sh" ];
   then
     exec /bin/bash -s
     exit $?
