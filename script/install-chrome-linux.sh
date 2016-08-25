b286eb10c8a86d7c7c04a72b06202e833d8a3283
Zhuohuan LI <zixia@zixia.net>
2016-08-25 12:04:29 +0800

    chrome & xvfb installer

--- script/install-chrome-linux.sh
+++ script/install-chrome-linux.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+[ "$(which google-chrome)" = "" ] && {
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+	sudo apt-get update
+	sudo apt-get install -y google-chrome-stable
+}
+
 google-chrome --version
-
-sudo apt-get install -y xvfb
-
-export DISPLAY=':99.0'
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-
-
