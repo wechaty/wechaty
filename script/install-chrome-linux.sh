b328addb6cc3d2641d62299e4c4711e4dd9c44de
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-25 05:15:16 +0000

    google-chrome-stable instead of google-chrome ?

--- script/install-chrome-linux.sh
+++ script/install-chrome-linux.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-[ "$(which google-chrome)" = "" ] && {
+[ "$(which google-chrome-stable)" = "" ] && {
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 	sudo apt-get update
 	sudo apt-get install -y google-chrome-stable
 }
 
-google-chrome --version
+google-chrome-stable --version
