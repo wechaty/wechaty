201936b68c11bb18d1b6da8550285b2919253439
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-29 19:58:29 +0000

    compitable for furture args

--- script/build-docker.sh
+++ script/build-docker.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-if [ "$1" = "test" ]; then
-  exec docker run -ti --rm zixia/wechaty:test test
-fi
-
 if [ "$1" = "build" ] || [ "$1" = "" ]; then
   exec docker build -t zixia/wechaty:test .
 fi
 
-echo "unknown params"
+exec docker run -ti --rm zixia/wechaty:test $1
