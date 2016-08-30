cbccef0a6d307de3f088af8648f16e5c3ffce600
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-08-30 18:34:16 +0000

    skip docker xvfb test, because xvfb will be auto started with xvfb-chromium

--- test/docker.spec.js
+++ test/docker.spec.js
@@ -3,15 +3,13 @@ import { test } from 'ava'
 import { execSync } from 'child_process'
 import sinon from 'sinon'
 
-import { log } from '../'
+import { Config, log } from '../'
 
-const docker = !!process.env.WECHATY_DOCKER
-
-!docker && test('Docker test skipped', function(t) {
+!Config.isDocker && test('Docker test skipped', function(t) {
   t.pass('not in docker, skip docker tests')
 })
 
-docker && test('Docker smoking test', function(t) {
+Config.isDocker && test.skip('Docker smoking test', function(t) {
   const n = execSync('ps a | grep Xvfb | grep -v grep | wc -l').toString().replace(/\n/, '', 'g')
   t.is(parseInt(n), 1, 'should has Xvfb started')
 })
