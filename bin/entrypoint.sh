310fc25ae04ba82765279fd36a3aa34caebadfbc
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-09-01 02:42:35 +0000

    code clean up, v0.3.15

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
-# Credit: https://github.com/cusspvz/node.docker/blob/master/entrypoint
+# Wechaty - Connect ChatBots
+#
+# https://github.com/wechaty/wechaty
+#
+# Original Code Credit: https://github.com/cusspvz/node.docker/blob/master/entrypoint
 #
 set -e
 
@@ -10,17 +14,18 @@ echo
 
 # to identify run env (for tests)
 export WECHATY_DOCKER='docker'
+# set CI here, in order to force ava to output use --verbose param, which is fit docker console log
+export CI="FORCE_AVA_OUTPUT_VERBOSE"
 
-# CI will force ava to output use --verbose param, which is fit docker console log
-export CI="$WECHATY_DOCKER"
+# [ "$WECHATY_HEAD" != "" ] && {
+#   echo "WECHATY_HEAD=$WECHATY_HEAD"
+# }
 
-echo "WECHATY_HEAD=$WECHATY_HEAD"
-
-if [ "$WECHATY_HEAD" != "phantomjs" ]; then
-  export DISPLAY=':99.0'
-  Xvfb :99 -ac -screen 0 640x480x8 -nolisten tcp &
-  echo "Xvfb started on DISPLAY=$DISPLAY"
-fi
+# if [ "$WECHATY_HEAD" != "phantomjs" ]; then
+#   export DISPLAY=':99.0'
+#   Xvfb :99 -ac -screen 0 640x480x8 -nolisten tcp &
+#   echo "Xvfb started on DISPLAY=$DISPLAY"
+# fi
 
 if [ "$1" = "start" ]; then
   exec npm start
