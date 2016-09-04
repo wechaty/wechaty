96bde2d1fb977cd69003fe5e9db77c4606058afd
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-09-05 00:38:12 +0800

    show name right

--- src/io-bot.js
+++ src/io-bot.js
@@ -46,7 +46,7 @@ class IoBot {
     })
 
     wechaty
-      .on('login'	       , user => this.log.info('IoBot', `${user.name} logined`))
+      .on('login'	       , user => this.log.info('IoBot', `${user.name()} logined`))
       .on('logout'	     , user => this.log.info('IoBot', `${user} logouted`))
       .on('scan', ({url, code}) => this.log.info('IoBot', `[${code}] ${url}`))
       .on('message'   , message => {
