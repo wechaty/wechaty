0f3a5be7c3fd2b569b99103da970017f92634570
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-09 21:56:08 +0800

    #40 Typescript transition step 17: fix index.ts & tslint

--- src/puppet-web/index.spec.ts
+++ src/puppet-web/index.spec.ts
@@ -1,14 +1,18 @@
 import { test }   from 'ava'
-import PuppetWeb  from './'
+import {
+    Bridge
+  , Browser
+  , Event
+  , PuppetWeb
+  , Server
+  , Watchdog
+} from './'
 
 test('PuppetWeb Module Exports', t => {
-  t.truthy(PuppetWeb.default    , 'should export default')
-  t.truthy(PuppetWeb.PuppetWeb  , 'should export PuppetWeb')
-  t.truthy(PuppetWeb.Event      , 'should export Event')
-  t.truthy(PuppetWeb.Watchdog   , 'should export Watchdog')
-  t.truthy(PuppetWeb.Server     , 'should export Server')
-  t.truthy(PuppetWeb.Browser    , 'should export Browser')
-  t.truthy(PuppetWeb.Bridge     , 'should export Bridge')
+  t.truthy(PuppetWeb  , 'should export PuppetWeb')
+  t.truthy(Event      , 'should export Event')
+  t.truthy(Watchdog   , 'should export Watchdog')
+  t.truthy(Server     , 'should export Server')
+  t.truthy(Browser    , 'should export Browser')
+  t.truthy(Bridge     , 'should export Bridge')
 })
-
-
