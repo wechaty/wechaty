5e588a13a44e122fc59868d424e03250e53bc982
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-17 12:33:03 +0800

    loggig

--- src/state-monitor.ts
+++ src/state-monitor.ts
@@ -10,6 +10,7 @@
  *
  * Helper Class for Manage State Change
  */
+import { log } from './config'
 
 /**
  * A - State A
@@ -21,7 +22,9 @@ export class StateMonitor <A, B>{
   private _current:  A|B
   private _stable:  boolean
 
-  constructor(initState: A|B) {
+  constructor(private client: string, initState: A|B) {
+    log.verbose('StateMonitor', 'constructor(%s, %s)', client, initState)
+
     this.target(initState)
     this.current(initState)
     this.stable(true)
@@ -29,22 +32,42 @@ export class StateMonitor <A, B>{
 
   public target(newState?: A|B): A|B {
     if (newState) {
+      log.verbose('StateMonitor', 'target(%s) %s state change from %s to %s'
+                                , this.client, newState
+                                , this._target, newState
+                )
       this._target = newState
+    } else {
+      log.verbose('StateMonitor', 'target() %s state is %s', this.client, this._target)
     }
     return this._target
   }
 
   public current(newState?: A|B, stable = true): A|B {
     if (newState) {
+      log.verbose('StateMonitor', 'current(%s, %s) %s state change from %s:%s to %s:%s'
+                                , newState, stable
+                                , this.client
+                                , this._current, this._stable
+                                , newState, stable
+                )
       this._current = newState
-      this.stable(stable)
+      this._stable  = stable
+    } else {
+      log.verbose('StateMonitor', 'current() %s state is %s', this.client, this._current)
     }
     return this._current
   }
 
-  public stable(isStable?: boolean) {
-    if (typeof isStable === 'boolean') {
-      this._stable = isStable
+  public stable(stable?: boolean) {
+    if (typeof stable === 'boolean') {
+      log.verbose('StateMonitor', 'stable(%s) %s state change from %s to %s'
+                                , stable
+                                , this.client
+                                , this._stable, stable)
+      this._stable = stable
+    } else {
+      log.verbose('StateMonitor', 'stable() %s state is %s', this.client, this._stable)
     }
     return this._stable
   }
