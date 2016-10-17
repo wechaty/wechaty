7eb9b043e6f9b8e03a2b7eec7dee4156034061f4
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-18 01:07:23 +0800

    add pre-push hook script

---
+++ bin/pre-push.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# An example hook script to verify what is about to be committed.
+# Called by "git commit" with no arguments.  The hook should
+# exit with non-zero status after issuing an appropriate message if
+# it wants to stop the commit.
+#
+# To enable this hook, rename this file to "pre-commit".
+
+npm run lint
