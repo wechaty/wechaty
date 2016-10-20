1941a7e3b4b7d3a16a56045b9db7930e1ddb49fb
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-20 15:13:52 +0800

    add `npm run doctor` to output diagnose information

---
+++ bin/doctor.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env ts-node
+
+import {
+    Config
+  , Wechaty
+} from '../'
+import * as os from 'os'
+
+const wechaty = Wechaty.instance()
+
+console.log(`
+
+# Wechaty Doctor
+
+1. Wechaty version: ${wechaty.version()}
+2. ${os.type()} ${os.arch()} version ${os.release()} free memory ${Math.floor(os.freemem() / 1024 / 1024)} MB
+3. Docker: ${Config.isDocker}
+
+`)
