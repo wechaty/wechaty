369ef8bb077fd9094ee4611b3d6971c02cdc6428
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-10-28 07:36:40 +0000

    fix docker.sh in circleci.com: must set image tag or will fail

--- script/docker.sh
+++ script/docker.sh
@@ -3,14 +3,15 @@
 # 1. CircleCI with `Btrfs volume error`
 #   https://circleci.com/docs/docker-btrfs-error/
 #
+imageName='wechaty:test'
 
 optRm='--rm'
 [ -n "$CIRCLECI" ] && optRm='--rm=false'
 
 if [ "$1" = "build" ] || [ "$1" = "" ]; then
-  exec docker build "$optRm" -t wechaty .
+  exec docker build "$optRm" -t "$imageName" .
   exit $?
 fi
 
-exec docker run -ti "$optRm" -v /dev/shm:/dev/shm wechaty $@
+exec docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" $@
 exit $?
