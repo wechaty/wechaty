530863def50805625eb0b18a4c9a5a2ec2edc86d
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-11-07 12:16:25 +0800

    doc

--- script/docker.sh
+++ script/docker.sh
@@ -11,6 +11,7 @@ optRm='--rm'
 [ -n "$CIRCLECI" ] && optRm='--rm=false'
 
 declare -i ret=0
+
 case "$1" in
   build | '')
     echo docker build "$optRm" -t "$imageName" .
@@ -19,17 +20,26 @@ case "$1" in
     ;;
 
   test)
+    #
+    # 1. test JavaScript(nodejs): if could run `js-bot.js``
+    #
     echo docker run -ti "$optRm" -v /dev/shm:/dev/shm -v "$(pwd)":/bot "$imageName" test/docker-bot/js-bot.js
     docker run -ti "$optRm" -v /dev/shm:/dev/shm -v "$(pwd)":/bot "$imageName" test/docker-bot/js-bot.js
 
+    #
+    # 2. test TypeScript(ts-node): if could run `ts-bot.ts``
+    #
     echo docker run -ti "$optRm" -v /dev/shm:/dev/shm -v "$(pwd)":/bot "$imageName" test/docker-bot/ts-bot.ts
     docker run -ti "$optRm" -v /dev/shm:/dev/shm -v "$(pwd)":/bot "$imageName" test/docker-bot/ts-bot.ts
 
-    echo docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" "$@"
-    exec docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" "$@"
+    #
+    # 3. run npm test
+    #
+    echo docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" test
+    exec docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" test
     ret=$?
     ;;
-    
+
   *)
     echo docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" "$@"
     exec docker run -ti "$optRm" -v /dev/shm:/dev/shm "$imageName" "$@"
