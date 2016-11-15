d724e62cdbd9118aa238f02e9566a9b53b5da23a
Huan LI <zixia@zixia.net>
2016-11-15 02:08:34 +0000

    use `ssh-agent` like env set

--- script/xvfb.sh
+++ script/xvfb.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 [ "$(which Xvfb)" = "" ] && {
 	sudo apt-get -qq update
 	sudo apt-get -qqy install xvfb
-	echo "Install Xvfb done"
+	echo 1>&2 "Install Xvfb done"
 }
 
-[ "$(ps a | grep Xvfb | grep -v grep | wc -l)" = 0 ] && {
+[ "$(pgrep -c Xvfb)" = 0 ] && {
 	Xvfb :99 -screen 0 640x480x8 > /dev/null 2>&1 &
-	echo "Start Xvfb done"
+	echo 1>&2 "Start Xvfb done"
 }
+
+echo 1>&2 "Xvfb ready."
+echo 1>&2
 
 [ "$DISPLAY" = "" ] && {
-	export DISPLAY=':99.0'
-  echo "Set DISPLAY=:99.0 done"
+  echo "export DISPLAY=:99.0"
+  echo 1>&2
 }
-
-echo "Xvfb ready."
