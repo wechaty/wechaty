ae65b2a5a036b569c34cc7a4c693f4006a51ec21
lijiarui <ruiruibupt@126.com>
2017-01-22 15:28:55 +0800

    test

--- src/room.ts
+++ src/room.ts
@@ -355,7 +355,7 @@ export class Room extends EventEmitter implements Sayable {
   public nick(contact: Contact): string {
     log.silly('Room', 'nick(%s)', contact.id)
     if (!this.obj || !this.obj.nickMap) {
-      return ''
+      return 'fuck'
     }
     return this.obj.displayMap[contact.id] || this.obj.nickMap[contact.id]
   }
