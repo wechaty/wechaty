4e1fa4504ec282c71bd0f5fbb25e311f12ea1f9c
Huan LI <zixia@zixia.net>
2017-04-30 20:10:08 +0800

    fix(brolog) use lowercase

--- src/brolog-env.ts
+++ src/brolog-env.ts
@@ -13,8 +13,7 @@ const level = process.env['WECHATY_LOG']
 const levelRegexStr = 'silly|verbose|info|warn|error|silent'
 const levelRegex = new RegExp(levelRegexStr, 'i')
 if (levelRegex.test(level)) {
-  // log.level = level.toLowerCase()
-  log.level(level)
+  log.level(level.toLowerCase())
   log.silly('Brolog', 'WECHATY_LOG set level to %s', level)
 } else if (level) {
   log.warn('Brolog', 'env WECHATY_LOG(%s) must be one of silly|verbose|info|warn|error|silent', level)
