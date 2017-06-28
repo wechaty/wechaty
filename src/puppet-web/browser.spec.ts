b4330b7835953e4d80a07cdcfb65cf89fd95de66
Huan LI <zixia@zixia.net>
2017-06-29 02:53:33 +0800

    fix(throws): use await to confirm promise resolved before end of test

--- src/puppet-web/browser.spec.ts
+++ src/puppet-web/browser.spec.ts
@@ -24,15 +24,15 @@ test.serial('quit()', async t => {
   const browser = new Browser()
   await browser.driver.init() // init driver, not init browser
 
-  t.throws(browser.quit(), Error, 'should throw on an un-inited browser')
+  await t.throws(browser.quit(), Error, 'should throw on an un-inited browser')
 
   browser.state.target('open')
   browser.state.current('open', false)
-  t.notThrows(browser.quit(), 'should not throw exception when call quit() on an `inprocess` `open` state browser')
+  await t.notThrows(browser.quit(), 'should not throw exception when call quit() on an `inprocess` `open` state browser')
 
   browser.state.target('close')
   browser.state.current('close')
-  t.throws(browser.quit(), Error, 'should throw exception when call quit() twice on browser')
+  await t.throws(browser.quit(), Error, 'should throw exception when call quit() twice on browser')
 })
 
 test.serial('init()', async t => {
@@ -40,10 +40,10 @@ test.serial('init()', async t => {
   browser.state.target('open')
 
   browser.state.current('open')
-  t.throws(browser.init(), Error, 'should throw exception when call init() on an `open` state browser')
+  await t.throws(browser.init(), Error, 'should throw exception when call init() on an `open` state browser')
 
   browser.state.current('open', false)
-  t.throws(browser.init(), Error, 'should throw exception when call init() on a `open`-`ing` state browser')
+  await t.throws(browser.init(), Error, 'should throw exception when call init() on a `open`-`ing` state browser')
 
   await browser.quit()
   t.pass('should quited browser')
