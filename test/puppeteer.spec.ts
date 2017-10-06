48e7a5d991264881f0b8108d71cb07db6b6b51ba
Huan LI <zixia@zixia.net>
2017-10-06 19:13:47 +0800

    add puppeteer demo usage

--- test/puppeteer.spec.ts
+++ test/puppeteer.spec.ts
@@ -17,10 +17,13 @@
  *
  */
 import * as path from 'path'
+
 // tslint:disable:no-shadowed-variable
-import * as test from 'blue-tape'
+import * as test  from 'blue-tape'
+import * as sinon from 'sinon'
 
 import {
+  Cookie,
   launch,
 }                 from 'puppeteer'
 
@@ -88,5 +91,143 @@ test('injectFile() a file and get the returns value', async t => {
 
   } catch (e) {
     t.fail(e && e.message || e)
+  }
+})
+
+test('page.on(console)', async t => {
+  const EXPECTED_ARG1 = 'arg1'
+  const EXPECTED_ARG2 = 2
+  const EXPECTED_ARG3 = { arg3: 3 }
+
+  const browser = await launch()
+  const page    = await browser.newPage()
+
+  const spy = sinon.spy()
+
+  page.on('console', spy)
+  await (page.evaluate as any)(function () {
+    console.log.apply(console, arguments)
+  }, EXPECTED_ARG1, EXPECTED_ARG2, EXPECTED_ARG3)
+
+  // wait a while to let chrome fire the event
+  await new Promise(r => setTimeout(r, 3))
+
+  t.ok(spy.calledOnce, 'should be called once')
+  t.equal(spy.args[0][0], EXPECTED_ARG1, 'should get console.log 1st arg')
+  t.equal(spy.args[0][1], EXPECTED_ARG2, 'should get console.log 2nd arg')
+  t.deepEqual(spy.args[0][2], EXPECTED_ARG3, 'should get console.log 3rd arg')
+
+  await page.close()
+  await browser.close()
+})
+
+test('page.exposeFunction()', async t => {
+  const browser = await launch()
+  const page    = await browser.newPage()
+
+  const spy = sinon.spy()
+
+  await page.exposeFunction('nodeFunc', spy)
+  await page.evaluate(`nodeFunc(42)`)
+  t.ok(spy.calledOnce, 'should be called once inside browser')
+  t.equal(spy.firstCall.args[0], 42, 'should be called with 42')
+
+  await page.close()
+  await browser.close()
+})
+
+test('other demos', async t => {
+  const EXPECTED_URL = 'https://www.google.com/'
+
+  try {
+    const browser = await launch({
+      ignoreHTTPSErrors: true,
+      headless: true,
+      args: [
+        '--allow-insecure-localhost',
+      ],
+    })
+
+    const version = await browser.version()
+    t.ok(version, 'should get version')
+
+    const page = await browser.newPage()
+    await page.goto(EXPECTED_URL)
+    // await page.goto('https://www.chromestatus.com/features', {waitUntil: 'networkidle'});
+    // await page.waitForSelector('h3 a');
+    // await page.click('input[type="submit"]');
+
+    // not the same with the document of ConsoleMessage???
+
+    page.on('dialog', async dialog => {
+      console.log(dialog)
+      console.log('dialog:', dialog.type, dialog.message())
+      await dialog.accept('ok')
+    })
+
+    page.on('error', (e, ...args) => {
+      console.error('error', e)
+      console.error('error:args:', args)
+    })
+    page.on('pageerror', (e, ...args) => {
+      console.error('pageerror', e)
+      console.error('pageerror:args:', args)
+    })
+
+    page.on('load', (e, ...args) => {
+      console.log('load:e:', e)
+      console.log('load:args:', args)
+    })
+
+    await page.setRequestInterceptionEnabled(true)
+    page.on('request', interceptedRequest => {
+      if (interceptedRequest.url.endsWith('.png')
+        || interceptedRequest.url.endsWith('.jpg')
+      ) {
+        interceptedRequest.abort()
+      } else {
+        interceptedRequest.continue()
+      }
+    })
+
+    page.on('requestfailed', (...args) => {
+      console.log('requestfailed:args:', args)
+    })
+
+    page.on('response', (res, ...args) => {
+      console.log('response:res:', res)
+      console.log('response:args:', args)
+    })
+
+    // page.click(selector[, options])
+    // await page.injectFile(path.join(__dirname, 'wechaty-bro.js'))
+    const cookieList = await page.cookies() as any as Cookie[]
+    t.ok(cookieList.length, 'should get cookies')
+    t.ok(cookieList[0].name, 'should get cookies with name')
+
+    const cookie: Cookie = {
+      name:  'test-name',
+      value: 'test-value',
+      domain: 'qq.com',
+      path: '/',
+      expires: 1234324132,
+      httpOnly: false,
+      secure: false,
+      sameSite: 'Strict',
+    }
+    await page.setCookie(cookie)
+
+    const result = await page.evaluate(() => 8 * 7)
+    t.equal(result, 56, 'should evaluated function for () => 8 * 7 = 56')
+
+    t.equal(await page.evaluate('1 + 2'), 3, 'should evaluated 1 + 2 = 3')
+
+    t.equal(await page.url(), EXPECTED_URL, 'should get the url right')
+    // await new Promise(r => setTimeout(r, 3000))
+
+    await page.close()
+    await browser.close()
+  } catch (e) {
+    t.fail(e)
   }
 })
