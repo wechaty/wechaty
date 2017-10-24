c9cccdb001700c936d513b311fe06930445de10d
Huan LI <zixia@zixia.net>
2017-10-25 03:40:32 +0800

    fix profile ext

--- src/profile.ts
+++ src/profile.ts
@@ -28,8 +28,11 @@ export class Profile {
         ? name
         : path.join(
             process.cwd(),
-            name + '.wechaty.json',
+            name,
           )
+      if (!/\.wechaty\.json$/.test(this.file)) {
+        this.file +=  '.wechaty.json'
+      }
     }
   }
 
