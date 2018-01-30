1691ab77aa9fd3b752d6fafcab38b0f6e6a45ff7
Huan LI <zixia@zixia.net>
2018-01-30 19:25:16 +0800

    add doc

--- script/safe-test.sh
+++ script/safe-test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# https://github.com/Chatie/wechaty/issues/1084
+# WebDriver / Puppeteer sometimes will fail(i.e. timeout) with no reason.
+# That will cause the unit tests fail randomly.
+# So we need to retry again when unit tests fail,
+# and treat it's really fail after MAX_RETRY_NUM times.
+
 MAX_RETRY_NUM=3
 
 n=0
