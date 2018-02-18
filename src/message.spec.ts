4c736aa2ef09405638fc99f982465f37a749238f
Huan LI <zixia@zixia.net>
2018-02-18 16:40:26 +0800

    get rid of useless setTimeout

--- src/message.spec.ts
+++ src/message.spec.ts
@@ -214,10 +214,10 @@ test('mentioned()', async t => {
   const room13 = msg13.room()
   if (room13) {
     await room13.ready()
-    setTimeout(function () {
-      const mentionContactList13 = msg13.mentioned()
-      t.is(mentionContactList13.length, 0, '@_@ wow! my email is ruiruibupt@gmail.com in message should not be treat as contact')
-    }, 1 * 1000)
+    // setTimeout(function () {
+    const mentionContactList13 = msg13.mentioned()
+    t.is(mentionContactList13.length, 0, '@_@ wow! my email is ruiruibupt@gmail.com in message should not be treat as contact')
+    // }, 1 * 1000)
   }
 
   const msg21 = new Message(rawObj21)
