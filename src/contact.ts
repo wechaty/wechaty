6f5f53df7a59341dd89eb721f5bcd2863253bfd4
Huan LI <zixia@zixia.net>
2018-03-11 17:09:33 +0800

    update license to 2018

--- src/contact.ts
+++ src/contact.ts
@@ -1,8 +1,7 @@
 /**
- *
  *   Wechaty - https://github.com/chatie/wechaty
  *
- *   @copyright 2016-2017 Huan LI <zixia@zixia.net>
+ *   @copyright 2016-2018 Huan LI <zixia@zixia.net>
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
