284c732fc39fdef77dfe39deae0073d19a0e483b
Huan LI <zixia@zixia.net>
2018-04-21 12:41:08 +0800

    add helper script to set tag to next (#1158)

---
+++ scripts/package-public-config-tag-next.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env ts-node
+import * as fs    from 'fs'
+import * as path  from 'path'
+
+const PACKAGE_JSON = path.join(__dirname, '../package.json')
+
+const pkg = require(PACKAGE_JSON)
+
+pkg.publishConfig.tag = 'next'
+
+fs.writeFileSync(PACKAGE_JSON, JSON.stringify(pkg, null, 2))
+// console.log(JSON.stringify(pkg, null, 2))
