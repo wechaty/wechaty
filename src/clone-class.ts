cff5c4b882df1daeb47b5605c68bb90e60af7206
Huan LI <zixia@zixia.net>
2018-04-22 22:44:11 +0800

    cloneClass util prototype done. (#518)

---
+++ src/clone-class.ts
@@ -0,0 +1,21 @@
+
+/**
+ * Clone Class for easy savig Information into Static Properties
+ * https://github.com/Chatie/wechaty/issues/518
+ */
+
+// https://github.com/Microsoft/TypeScript/issues/10262
+// https://github.com/Microsoft/TypeScript/pull/13743
+type Constructor<T> = new(...args: any[]) => T
+
+// tslint:disable-next-line:variable-name
+export function cloneClass<T extends Constructor<{}>>(OrignalClass: T): T {
+  class NewClass extends OrignalClass {
+    constructor(...args: any[]) {
+      super(...arguments)
+    }
+  }
+  return NewClass as any as T
+}
+
+export default cloneClass
