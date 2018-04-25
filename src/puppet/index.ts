46f0adcc78024a679c74884bba9a9ddd9d4bb97a
Huan LI <zixia@zixia.net>
2018-04-25 20:21:39 +0800

    wip...

---
+++ src/puppet/index.ts
@@ -0,0 +1,25 @@
+export {
+  Contact,
+  ContactQueryFilter,
+}                         from './contact'
+export {
+  FriendRequest,
+}                         from './friend-request'
+export {
+  Message,
+}                         from './message'
+export {
+  Puppet,
+  PuppetEvent,
+  PuppetOptions,
+  ScanData,
+}                         from './puppet'
+export {
+  PuppetAccessory,
+}                         from './puppet-accessory'
+export {
+  Room,
+  RoomMemberQueryName,
+  RoomMemberQueryFilter,
+  RoomQueryFilter,
+}                         from './room'
