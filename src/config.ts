11c8c63eb53fbe7d73b153ab88531675c0444be2
Huan LI <zixia@zixia.net>
2018-07-10 10:06:53 +0800

    lint

--- src/config.ts
+++ src/config.ts
@@ -31,10 +31,6 @@ import {
   FileBox,
 }                 from 'file-box'
 
-import {
-  PuppetName,
-}             from './puppet-config'
-
 // https://github.com/Microsoft/TypeScript/issues/14151#issuecomment-280812617
 if (!Symbol.asyncIterator) {
   (<any>Symbol).asyncIterator = Symbol.for('Symbol.asyncIterator')
