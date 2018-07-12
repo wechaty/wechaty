503e6910d72f61b631ae8ded64a7efd0769a433f
Jiarui LI <ruiruibupt@126.com>
2018-07-12 12:07:36 +0800

    https://github.com/qhduan/wechaty-doc/issues/19

--- src/user/message.ts
+++ src/user/message.ts
@@ -718,9 +718,9 @@ export class Message extends Accessory implements Sayable {
   /**
    * Returns the message age in seconds. <br>
    *
-   * For example, the message is sent at time 8:43:01,
-   * and when we received it in Wechaty, the time is 8:43:15,
-   * then the age() will return 8:43:15 - 8:43:01 = 14 (seconds)
+   * For example, the message is sent at time `8:43:01`,
+   * and when we received it in Wechaty, the time is `8:43:15`,
+   * then the age() will return `8:43:15 - 8:43:01 = 14 (seconds)`
    * @returns {number}
    */
   public age(): number {
