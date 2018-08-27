6f25b0c74cab7413cc4b41137ede79c02a79a581
Huan LI <zixia@zixia.net>
2018-08-27 14:53:13 +0800

    add ioscat back

--- src/puppet-config.ts
+++ src/puppet-config.ts
@@ -6,7 +6,7 @@ export const PUPPET_DEPENDENCIES = {
   'padchat'                  : '0.0.0',    // compatible with v0.18, will be replaced with wechaty-puppet-padchat
   /////////////////////////////////////
   // 'wechaty-puppet-hostie'    : '^0.0.1',
-  // 'wechaty-puppet-ioscat'    : '^0.5',
+  'wechaty-puppet-ioscat'    : '^0.5.10',
   'wechaty-puppet-mock'      : '^0.14.1',
   'wechaty-puppet-padchat'   : '^0.14.1',
   'wechaty-puppet-puppeteer' : '^0.14.1',
