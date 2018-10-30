a5ea9eac1f003736daa7c31fc8ec1077c387ec56
Yuan Gao <wind.memory.cn@gmail.com>
2018-10-30 19:46:19 +0800

    add new puppet wechaty-puppet-padpro

--- src/puppet-config.ts
+++ src/puppet-config.ts
@@ -5,11 +5,13 @@ export const PUPPET_DEPENDENCIES = {
   'default'                  : '0.0.0',    // will be replaced with PUPPET_DEFAULT
   'mock'                     : '0.0.0',    // compatible with v0.18, will be replaced with wechaty-puppet-padchat
   'padchat'                  : '0.0.0',    // compatible with v0.18, will be replaced with wechaty-puppet-padchat
+  'padpro'                   : '0.0.0',
   /////////////////////////////////////
   // 'wechaty-puppet-hostie'    : '^0.0.1',
   'wechaty-puppet-ioscat'    : '^0.5.19',
   'wechaty-puppet-mock'      : '^0.14.1',
   'wechaty-puppet-padchat'   : '^0.17.2',
+  'wechaty-puppet-padpro'    : '^0.0.1',
   'wechaty-puppet-puppeteer' : '^0.14.1',
   'wechaty-puppet-wechat4u'  : '^0.14.1',
 }
