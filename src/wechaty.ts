422c10701968632cc15161fdae6e579955c8dc98
lijiarui <ruiruibupt@126.com>
2018-11-01 21:08:37 +0800

    move leave comment to a right place

--- src/wechaty.ts
+++ src/wechaty.ts
@@ -327,19 +327,19 @@ export class Wechaty extends Accessory implements Sayable {
   /**
    * @desc       Wechaty Class Event Type
    * @typedef    WechatyEventName
-   * @property   {string}  error      - When the bot get error, there will be a Wechaty error event fired.
-   * @property   {string}  login      - After the bot login full successful, the event login will be emitted, with a Contact of current logined user.
-   * @property   {string}  logout     - Logout will be emitted when bot detected log out, with a Contact of the current login user.
-   * @property   {string}  heartbeat  - Get bot's heartbeat.
-   * @property   {string}  friendship - When someone sends you a friend request, there will be a Wechaty friendship event fired.
-   * @property   {string}  message    - Emit when there's a new message.
-   * @property   {string}  ready      - Emit when all data has load completed, in wechaty-puppet-padchat, it means it has sync Contact and Room completed
-   * @property   {string}  room-join  - Emit when anyone join any room.
-   * @property   {string}  room-topic - Get topic event, emitted when someone change room topic.
-   * @property   {string}  room-leave - Emit when anyone leave the room.<br>
+   * @property   {string}  error       - When the bot get error, there will be a Wechaty error event fired.
+   * @property   {string}  login       - After the bot login full successful, the event login will be emitted, with a Contact of current logined user.
+   * @property   {string}  logout      - Logout will be emitted when bot detected log out, with a Contact of the current login user.
+   * @property   {string}  heartbeat   - Get bot's heartbeat.
+   * @property   {string}  friendship  - When someone sends you a friend request, there will be a Wechaty friendship event fired.
+   * @property   {string}  message     - Emit when there's a new message.
+   * @property   {string}  ready       - Emit when all data has load completed, in wechaty-puppet-padchat, it means it has sync Contact and Room completed
+   * @property   {string}  room-join   - Emit when anyone join any room.
+   * @property   {string}  room-topic  - Get topic event, emitted when someone change room topic.
+   * @property   {string}  room-leave  - Emit when anyone leave the room.<br>
+   *                                   - If someone leaves the room by themselves, wechat will not notice other people in the room, so the bot will never get the "leave" event.
    * @property   {string}  room-invite - Emit when there is a room invitation, see more in  {@link RoomInvitation}
-   *                                    If someone leaves the room by themselves, wechat will not notice other people in the room, so the bot will never get the "leave" event.
-   * @property   {string}  scan       - A scan event will be emitted when the bot needs to show you a QR Code for scanning. </br>
+   * @property   {string}  scan        - A scan event will be emitted when the bot needs to show you a QR Code for scanning. </br>
    *                                    It is recommend to install qrcode-terminal(run `npm install qrcode-terminal`) in order to show qrcode in the terminal.
    */
 
