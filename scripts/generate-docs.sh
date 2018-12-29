75e29dd57ed7a36a3d01efae668b649ef4424d20
Huan LI <zixia@zixia.net>
2018-12-29 19:14:52 +0800

    update docs

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -3,12 +3,12 @@ set -e
 
 npm version
 
-if [ "$1" != "dev" ] && ./scripts/development-release.ts; then
+if [ "$1" != "-f" ] && ./scripts/development-release.ts; then
   echo "Current release is a development release, please only update the docs when there's a stable release."
   exit 1
 else
   echo "Generating docs ..."
   npm run dist
-  echo -e '# Wechaty v'$(jq -r .version package.json)' Documentation\n\n* Blog - <https://blog.chatie.io>\n* Docs - <https://docs.chatie.io>\n\n' > docs/index.md
+  echo -e '# Wechaty v'$(jq -r .version package.json)' Documentation\n\n- Blog - <https://blog.chatie.io>\n- Docs - <https://docs.chatie.io>\n\n' > docs/index.md
   jsdoc2md dist/src/wechaty.js dist/src/user/{room,contact,contact-self,friendship,message,room-invitation}.js >> docs/index.md
 fi
