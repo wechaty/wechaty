6f2e4b561893b8d07c0aa2d47f5c423174f7ac8d
linyimin-bupt <linyimin520812@gmail.com>
2019-04-22 14:52:07 +0800

    change indnetation to be two spaces

--- src/user/room.ts
+++ src/user/room.ts
@@ -431,10 +431,10 @@ export class Room extends Accessory implements Sayable {
    * // 6. send url link in a room
    *
    * const urlLink = new UrlLink ({
-   *  description : 'WeChat Bot SDK for Individual Account, Powered by TypeScript, Docker, and Love',
-   *  thumbnailUrl: 'https://avatars0.githubusercontent.com/u/25162437?s=200&v=4',
-   *  title       : 'Welcome to Wechaty',
-   *  url         : 'https://github.com/chatie/wechaty',
+   *   description : 'WeChat Bot SDK for Individual Account, Powered by TypeScript, Docker, and Love',
+   *   thumbnailUrl: 'https://avatars0.githubusercontent.com/u/25162437?s=200&v=4',
+   *   title       : 'Welcome to Wechaty',
+   *   url         : 'https://github.com/chatie/wechaty',
    * })
    * await room.say(urlLink)
    */
