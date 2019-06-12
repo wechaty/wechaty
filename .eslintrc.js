32323b225f089589b270e669876fceb24138453d
Huan LI <zixia@zixia.net>
2019-06-12 22:27:48 +0800

    move rules to eslint-config in chatie

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,4 @@
-// {
-//   "rules": {
-//       "indent": [
-//           "error",
-//           4,
-//           {
-//               "CallExpression": {
-//                   "arguments": "first"
-//               }
-//           }
-//       ]
-//   }
-// }
-
 const rules = {
-  // indent: ['error', 2, {
-  //   CallExpression: {
-  //     arguments: 'off',
-  //   },
-  //   SwitchCase: 1,
-  // }],
-  'no-useless-constructor': 'off',
-  '@typescript-eslint/no-useless-constructor': 'off',
-  'no-dupe-class-members': 'off',
-  'operator-linebreak': ['error', 'before'],
 }
 
 module.exports = {
