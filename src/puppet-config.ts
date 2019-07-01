0f0f2a609455fbae0dbefba4d156f4d0e4adaca3
Huan LI <zixia@zixia.net>
2019-07-01 15:04:58 +0800

    Upgrade padpro to next version

--- src/puppet-config.ts
+++ src/puppet-config.ts
@@ -15,7 +15,7 @@ export const PUPPET_DEPENDENCIES = {
   'wechaty-puppet-ioscat'    : '^0.5.19',   // https://www.npmjs.com/package/wechaty-puppet-ioscat
   'wechaty-puppet-mock'      : '^0.15.5',   // https://www.npmjs.com/package/wechaty-puppet-mock
   'wechaty-puppet-padchat'   : '^0.18.1',   // https://www.npmjs.com/package/wechaty-puppet-padchat
-  'wechaty-puppet-padpro'    : '^0.2.0',    // https://www.npmjs.com/package/wechaty-puppet-padpro
+  'wechaty-puppet-padpro'    : '^0.3.7',    // https://www.npmjs.com/package/wechaty-puppet-padpro
   'wechaty-puppet-puppeteer' : '^0.17.14',   // https://www.npmjs.com/package/wechaty-puppet-puppeteer
   'wechaty-puppet-wechat4u'  : '^0.14.1',   // https://www.npmjs.com/package/wechaty-puppet-wechat4u
 }
