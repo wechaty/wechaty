297dc4ff8f30c2bb833ebeba20290c4457d3a949
SuperChang <su_chang@yeah.net>
2020-02-18 10:10:09 +0800

    Support message.toImage() method. (#1913)
    * 0.29.3
    
    * feat: support message.toImage() method
    
    * 0.31.10
    
    * re-designed the Image class
    
    * 0.31.11
    
    Co-authored-by: Huan (李卓桓) <zixia@zixia.net>

---
+++ src/user/image.ts
@@ -0,0 +1,56 @@
+import { instanceToClass } from 'clone-class'
+import { FileBox } from 'file-box'
+
+import { ImageType } from 'wechaty-puppet'
+
+import { Accessory } from '../accessory'
+import { log }        from '../config'
+
+export class Image extends Accessory {
+
+  constructor (
+    public id: string,
+  ) {
+    super()
+    log.verbose('Image', 'constructor(%s)',
+      id,
+      this.constructor.name,
+    )
+
+    const MyClass = instanceToClass(this, Image)
+
+    if (MyClass === Image) {
+      throw new Error('Image class can not be instanciated directly! See: https://github.com/wechaty/wechaty/issues/1217')
+    }
+
+    if (!this.puppet) {
+      throw new Error('Image class can not be instanciated without a puppet!')
+    }
+  }
+
+  public static create (id: string): Image {
+    log.verbose('Image', 'static create(%s)', id)
+
+    const image = new Image(id)
+    return image
+  }
+
+  public async thumbnail (): Promise<FileBox> {
+    log.verbose('Image', 'thumbnail() for id: "%s"', this.id)
+    const fileBox = await this.puppet.messageImage(this.id, ImageType.Thumbnail)
+    return fileBox
+  }
+
+  public async hd (): Promise<FileBox> {
+    log.verbose('Image', 'hd() for id: "%s"', this.id)
+    const fileBox = await this.puppet.messageImage(this.id, ImageType.HD)
+    return fileBox
+  }
+
+  public async artwork (): Promise<FileBox> {
+    log.verbose('Image', 'artwork() for id: "%s"', this.id)
+    const fileBox = await this.puppet.messageImage(this.id, ImageType.Artwork)
+    return fileBox
+  }
+
+}
