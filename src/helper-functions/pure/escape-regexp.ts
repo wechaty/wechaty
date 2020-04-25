d050847f8565f9aeffc28a2a6ed2cf44deebe0e3
Huan LI (李卓桓) <zixia@zixia.net>
2020-04-25 12:04:01 +0800

    docs for escapeRegexp

--- src/helper-functions/pure/escape-regexp.ts
+++ src/helper-functions/pure/escape-regexp.ts
@@ -1,4 +1,7 @@
-// https://stackoverflow.com/a/3561711/1123955
+/**
+ * Is there a RegExp.escape function in Javascript?
+ *  https://stackoverflow.com/a/3561711/1123955
+ */
 export function escapeRegExp (text: string) {
   return text.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
