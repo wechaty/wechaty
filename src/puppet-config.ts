ffea96c97c5c462a81b9212d09422f0b8a77a519
Huan LI (李卓桓) <zixia@zixia.net>
2021-02-20 11:18:25 +0800

    upgrade upppeteer version

--- src/puppet-config.ts
+++ src/puppet-config.ts
@@ -43,29 +43,29 @@ export const PUPPET_DEPENDENCIES = {
   /**
    * Wechaty Internal Puppets: dependency by package.json
    */
-  'wechaty-puppet-service'   : '*',   // https://www.npmjs.com/package/wechaty-puppet-service
-  'wechaty-puppet-mock'      : '*',   // https://www.npmjs.com/package/wechaty-puppet-mock
+  'wechaty-puppet-service' : '*',   // https://www.npmjs.com/package/wechaty-puppet-service
+  'wechaty-puppet-mock'    : '*',   // https://www.npmjs.com/package/wechaty-puppet-mock
 
   /**
    * WeChat External Puppets
    */
-  'wechaty-puppet-puppeteer'        : '>=0.22.1',   // https://www.npmjs.com/package/wechaty-puppet-puppeteer
-  'wechaty-puppet-wechat4u'         : '>=0.17.7',   // https://www.npmjs.com/package/wechaty-puppet-wechat4u
-  'wechaty-puppet-padlocal'         : '>=0.2.31',   // https://www.npmjs.com/package/wechaty-puppet-padlocal
-  'wechaty-puppet-official-account' : '>=0.5.43',   // https://www.npmjs.com/package/wechaty-puppet-official-account
+  'wechaty-puppet-puppeteer'        : '>=0.24',  // https://www.npmjs.com/package/wechaty-puppet-puppeteer
+  'wechaty-puppet-wechat4u'         : '>=0.17',  // https://www.npmjs.com/package/wechaty-puppet-wechat4u
+  'wechaty-puppet-padlocal'         : '>=0.2',   // https://www.npmjs.com/package/wechaty-puppet-padlocal
+  'wechaty-puppet-official-account' : '>=0.5',   // https://www.npmjs.com/package/wechaty-puppet-official-account
 
   /**
    * Non-WeChat External Puppets
    */
-  'wechaty-puppet-gitter'           : '>=0.4.7',   // https://www.npmjs.com/package/wechaty-puppet-gitter
-  'wechaty-puppet-lark'             : '>=0.4.5',   // https://www.npmjs.com/package/wechaty-puppet-lark
-  'wechaty-puppet-whatsapp'         : '>=0.2.2',   // https://www.npmjs.com/package/wechaty-puppet-whatsapp
+  'wechaty-puppet-gitter'   : '>=0.4.7',   // https://www.npmjs.com/package/wechaty-puppet-gitter
+  'wechaty-puppet-lark'     : '>=0.4.5',   // https://www.npmjs.com/package/wechaty-puppet-lark
+  'wechaty-puppet-whatsapp' : '>=0.2.2',   // https://www.npmjs.com/package/wechaty-puppet-whatsapp
 
   /**
    * Scoped puppets (private)
    */
-  '@juzibot/wechaty-puppet-donut': '^0.3', // https://www.npmjs.com/package/wechaty-puppet-donut (to be published)
-  '@juzibot/wechaty-puppet-wxwork': '*',   // https://www.npmjs.com/package/wechaty-puppet-wxwork (to be published)
+  '@juzibot/wechaty-puppet-donut'  : '*',   // https://www.npmjs.com/package/wechaty-puppet-donut (to be published)
+  '@juzibot/wechaty-puppet-wxwork' : '*',   // https://www.npmjs.com/package/wechaty-puppet-wxwork (to be published)
 }
 
 export type PuppetModuleName = keyof typeof PUPPET_DEPENDENCIES
