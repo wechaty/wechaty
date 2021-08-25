51d54839f434077abda6806ed709b425f03b13aa
Huan (李卓桓) <zixia@zixia.net>
2021-08-25 15:17:05 +0800

    upgrade puppet

--- src/puppet-config.ts
+++ src/puppet-config.ts
@@ -44,8 +44,8 @@ export const PUPPET_DEPENDENCIES = {
   /**
    * Wechaty Internal Puppets: dependency by package.json
    */
-  'wechaty-puppet-service' : '>=0.29.5',  // https://www.npmjs.com/package/wechaty-puppet-service
-  'wechaty-puppet-mock'    : '>=0.29',    // https://www.npmjs.com/package/wechaty-puppet-mock
+  'wechaty-puppet-service' : '*',  // https://www.npmjs.com/package/wechaty-puppet-service
+  'wechaty-puppet-mock'    : '*',    // https://www.npmjs.com/package/wechaty-puppet-mock
 
   /**
    * WeChat Puppets
