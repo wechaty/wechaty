f9e5a72a7f1136f7fe24fe72cb1ab6fe9315ed13
Huan <zixia@zixia.net>
2021-08-29 00:33:51 +0800

    fix shell lint

--- scripts/npm-pack-testing.sh
+++ scripts/npm-pack-testing.sh
@@ -3,7 +3,7 @@ set -e
 
 VERSION=$(npx pkg-jq -r .version)
 
-if npx --package @chatie/semver semver-is-prod $VERSION; then
+if npx --package @chatie/semver semver-is-prod "$VERSION"; then
   NPM_TAG=latest
 else
   NPM_TAG=next
@@ -14,13 +14,13 @@ npm pack
 
 TMPDIR="/tmp/npm-pack-testing.$$"
 mkdir "$TMPDIR"
-mv *-*.*.*.tgz "$TMPDIR"
+mv ./*-*.*.*.tgz "$TMPDIR"
 cp tests/fixtures/smoke-testing.ts "$TMPDIR"
 
 cd $TMPDIR
 
 npm init -y
-npm install --production *-*.*.*.tgz \
+npm install --production ./*-*.*.*.tgz \
   @types/node \
   typescript@latest \
   "wechaty-puppet-mock@$NPM_TAG" \
@@ -49,9 +49,7 @@ node smoke-testing.js
 #
 # ES Modules
 #
-
-# https://stackoverflow.com/a/59203952/1123955
-echo $(jq '.type="module"' package.json) > package.json
+npx pkg-jq -i '.type="module"'
 
 ./node_modules/.bin/tsc \
   --target es2020 \
