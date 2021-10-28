6a7c4a0fa2e6fa395ce8611d737758a49fc9ca24
Huan (李卓桓) <zixia@zixia.net>
2021-10-29 02:11:01 +0800

    support import wechaty/users

---
+++ src/mods/impls.ts
@@ -0,0 +1,77 @@
+/**
+ *   Wechaty Chatbot SDK - https://github.com/wechaty/wechaty
+ *
+ *   @copyright 2016 Huan LI (李卓桓) <https://github.com/huan>, and
+ *                   Wechaty Contributors <https://github.com/wechaty>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+export type {
+  Contact,
+  ContactConstructor,
+  ContactSelf,
+  ContactSelfConstructor,
+  Delay,
+  DelayConstructor,
+  Favorite,
+  FavoriteConstructor,
+  Friendship,
+  FriendshipConstructor,
+  Image,
+  ImageConstructor,
+  Location,
+  LocationConstructor,
+  Message,
+  MessageConstructor,
+  MiniProgram,
+  MiniProgramConstructor,
+  Moment,
+  MomentConstructor,
+  Money,
+  MoneyConstructor,
+  Room,
+  RoomConstructor,
+  RoomInvitation,
+  RoomInvitationConstructor,
+  Tag,
+  TagConstructor,
+  UrlLink,
+  UrlLinkConstructor,
+}                               from '../user/mod.js'
+
+export {
+  ContactImpl,
+  ContactSelfImpl,
+  FavoriteImpl,
+  FriendshipImpl,
+  ImageImpl,
+  LocationImpl,
+  MessageImpl,
+  MiniProgramImpl,
+  MomentImpl,
+  MoneyImpl,
+  RoomImpl,
+  RoomInvitationImpl,
+  DelayImpl,
+  TagImpl,
+  UrlLinkImpl,
+}                             from '../user/mod.js'
+
+export {
+  WechatyImpl,
+}                         from '../wechaty.js'
+export type {
+  Wechaty,
+  WechatyConstructor,
+}                         from '../interface/mod.js'
