3386ed53dce7cf85be2a781e8d4f6d7b7d0c6aba
Huan (李卓桓) <zixia@zixia.net>
2021-11-01 15:00:33 +0800

    clean

--- src/wechaty.spec.ts
+++ src/wechaty.spec.ts
@@ -308,7 +308,7 @@ test('ready()', async t => {
   await wechaty.stop()
 })
 
-test.only('on/off event listener management', async t => {
+test('on/off event listener management', async t => {
   const puppet = new PuppetMock()
   const wechaty = new WechatyImpl({ puppet })
 
@@ -317,9 +317,8 @@ test.only('on/off event listener management', async t => {
 
   wechaty.on('message', onMessage)
   t.equal(wechaty.listenerCount('message'), 1, 'should +1 listener after on(message)')
-  await new Promise(setImmediate)
+
   wechaty.off('message', onMessage)
-  await new Promise(setImmediate)
   t.equal(wechaty.listenerCount('message'), 0, 'should -1 listener after off(message)')
 })
 
