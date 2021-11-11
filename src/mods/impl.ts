330afb4cc428f4c4dc777cee518e509c5882b7c1
Huan (李卓桓) <zixia@zixia.net>
2021-11-11 14:05:53 +0800

    export use singlar instead of plural

---
+++ src/mods/impl.ts
@@ -0,0 +1,112 @@
+/**
+ *   Wechaty Chatbot SDK - https://github.com/wechaty/wechaty
+ *
+ *   @copyright 2016 Huan LI (李卓桓) <https://github.com/huan>, and
+ *                   Wechaty Contributors <https://github.com/wechaty>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/**
+ * Huan(202111): We export all names with specific postfix to make it easier to be recongnized
+ *  with less misunderstandings.
+ *
+ * Postfixes:
+ *  - Interface:    type, the `interface
+ *  - Constructor:  type, the `Constructor<interface>`
+ *  - Impl:         value, the `class`
+ *
+ *  - no postfix:  value, the `class`
+ */
+
+export {
+  ContactImpl,
+  ContactSelfImpl,
+  FavoriteImpl,
+  FriendshipImpl,
+  ImageImpl,
+  LocationImpl,
+  MessageImpl,
+  MiniProgramImpl,
+  MomentImpl,
+  MoneyImpl,
+  RoomImpl,
+  RoomInvitationImpl,
+  DelayImpl,
+  TagImpl,
+  UrlLinkImpl,
+}                             from '../user-modules/mod.js'
+
+// export {
+//   ContactImpl         as Contact,
+//   ContactSelfImpl     as ContactSelf,
+//   FavoriteImpl        as Favorite,
+//   FriendshipImpl      as Friendship,
+//   ImageImpl           as Image,
+//   LocationImpl        as Location,
+//   MessageImpl         as Message,
+//   MiniProgramImpl     as MiniProgram,
+//   MomentImpl          as Moment,
+//   MoneyImpl           as Money,
+//   PostImpl            as Post,
+//   RoomImpl            as Room,
+//   RoomInvitationImpl  as RoomInvitation,
+//   DelayImpl           as Delay,
+//   TagImpl             as Tag,
+//   UrlLinkImpl         as UrlLink,
+// }                                         from '../user-modules/mod.js'
+
+export type {
+  ContactInterface,
+  ContactSelfInterface,
+  DelayInterface,
+  FavoriteInterface,
+  FriendshipInterface,
+  ImageInterface,
+  LocationInterface,
+  MessageInterface,
+  MiniProgramInterface,
+  MomentInterface,
+  MoneyInterface,
+  RoomInterface,
+  RoomInvitationInterface,
+  TagInterface,
+  UrlLinkInterface,
+}                               from '../user-modules/mod.js'
+
+export type {
+  ContactConstructor,
+  ContactSelfConstructor,
+  DelayConstructor,
+  FavoriteConstructor,
+  FriendshipConstructor,
+  ImageConstructor,
+  LocationConstructor,
+  MessageConstructor,
+  MiniProgramConstructor,
+  MomentConstructor,
+  MoneyConstructor,
+  RoomConstructor,
+  RoomInvitationConstructor,
+  TagConstructor,
+  UrlLinkConstructor,
+}                               from '../user-modules/mod.js'
+
+export {
+  WechatyImpl,
+}                         from '../wechaty/mod.js'
+export type {
+  WechatyInterface,
+  WechatyConstructor,
+}                         from '../wechaty/mod.js'
---
+++ src/mods/user.ts
@@ -0,0 +1,43 @@
+/**
+ *   Wechaty Chatbot SDK - https://github.com/wechaty/wechaty
+ *
+ *   @copyright 2016 Huan LI (李卓桓) <https://github.com/huan>, and
+ *                   Wechaty Contributors <https://github.com/wechaty>.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+/**
+ * Huan(202111): we export all `interface` from this file, as the most convenience class name.
+ *
+ *  since they are `interface`s, so they are `type`-only.
+ */
+
+export type {
+  ContactInterface        as Contact,
+  ContactSelfInterface    as ContactSelf,
+  DelayInterface          as Delay,
+  FavoriteInterface       as Favorite,
+  FriendshipInterface     as Friendship,
+  ImageInterface          as Image,
+  LocationInterface       as Location,
+  MessageInterface        as Message,
+  MiniProgramInterface    as MiniProgram,
+  MomentInterface         as Moment,
+  MoneyInterface          as Money,
+  RoomInterface           as Room,
+  RoomInvitationInterface as RoomInvitation,
+  TagInterface            as Tag,
+  UrlLinkInterface        as UrlLink,
+}                                               from '../user-modules/mod.js'
