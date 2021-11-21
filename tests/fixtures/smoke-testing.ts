dc2f324682011c783c03785ecb3bd4528a41db57
Huan (李卓桓) <zixia@zixia.net>
2021-11-22 01:51:19 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -24,8 +24,8 @@ import {
   VERSION,
 }                   from 'wechaty'
 
-import * as USER from 'wechaty/user'
-import * as IMPL from 'wechaty/impl'
+import * as USERS from 'wechaty/users'
+import * as IMPLS from 'wechaty/impls'
 
 import assert from 'assert'
 
@@ -73,8 +73,8 @@ async function main () {
     )
   }
 
-  const tag: USER.Tag = {} as any as IMPL.TagImpl
-  assert.ok(tag, 'should get valid USER & IMPL')
+  const tag: USERS.Tag = {} as any as IMPLS.TagImpl
+  assert.ok(tag, 'should get valid USERS & IMPLS')
 
   assert.notStrictEqual(VERSION,  '0.0.0', 'VERSION must be set!')
 
