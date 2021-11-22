5ce1bc78d81fc3c306624f76cae3ce09d88cc109
Huan (李卓桓) <zixia@zixia.net>
2021-11-22 16:19:13 +0800

    use fallback  values when create urlLink

--- src/user-modules/url-link.ts
+++ src/user-modules/url-link.ts
@@ -38,25 +38,33 @@ class UrlLinkMixin extends wechatifyMixinBase() {
    * Create from URL
    *
    */
-  static async create (url: string): Promise<UrlLinkInterface> {
+  static async create (
+    url: string,
+    fallback?: Partial<
+      Omit<
+        PUPPET.payloads.UrlLink,
+        'url'
+      >
+    >
+  ): Promise<UrlLinkInterface> {
     log.verbose('UrlLink', 'create(%s)', url)
 
     const meta = await openGraph(url)
 
-    let description: string | undefined
-    let imageUrl: string | undefined
-    let title: string
+    let description  : undefined | string
+    let thumbnailUrl : undefined | string
+    let title        : string
 
     if (meta.image) {
       if (typeof meta.image === 'string') {
-        imageUrl = meta.image
+        thumbnailUrl = meta.image
       } else if (Array.isArray(meta.image)) {
-        imageUrl = meta.image[0]
+        thumbnailUrl = meta.image[0]
       } else {
         if (Array.isArray(meta.image.url)) {
-          imageUrl = meta.image.url[0]
+          thumbnailUrl = meta.image.url[0]
         } else if (meta.image.url) {
-          imageUrl = meta.image.url
+          thumbnailUrl = meta.image.url
         }
       }
     }
@@ -75,19 +83,15 @@ class UrlLinkMixin extends wechatifyMixinBase() {
       description = title
     }
 
-    if (!imageUrl || !description) {
-      throw new Error(`imageUrl(${imageUrl}) or description(${description}) not found!`)
-    }
-
-    if (!imageUrl.startsWith('http')) {
-      const resolvedUrl = new Url.URL(imageUrl, url)
-      imageUrl = resolvedUrl.toString()
+    if (thumbnailUrl && !thumbnailUrl.startsWith('http')) {
+      const resolvedUrl = new Url.URL(thumbnailUrl, url)
+      thumbnailUrl = resolvedUrl.toString()
     }
 
     const payload: PUPPET.payloads.UrlLink = {
-      description,
-      thumbnailUrl: imageUrl,
-      title,
+      description  : description  || fallback?.description  || '',
+      thumbnailUrl : thumbnailUrl || fallback?.thumbnailUrl || '',
+      title        : title        || fallback?.title        || '',
       url,
     }
 
