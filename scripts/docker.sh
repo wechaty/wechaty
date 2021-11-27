b06bee1b64f3fb8f090edadaf37ba03abe5b07ad
Huan (李卓桓) <zixia@zixia.net>
2021-11-27 11:42:06 +0800

    enhance docker build script to support multi platform

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,77 +2,55 @@
 
 set -eo pipefail
 
-function deployVersion () {
-  ARTIFACT_IMAGE=$1
-  IMAGE=$2
-  TAG=$3
+ARTIFACT_IMAGE='wechaty-artifact'
 
-  echo "Deploying TAG=$TAG"
-  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}:${TAG}"
-  docker push "${IMAGE}:${TAG}"
-}
-
-function deployLatest () {
-  ARTIFACT_IMAGE=$1
-  IMAGE=$2
-
-  echo "Deploying IMAGE=$IMAGE latest"
-  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}:latest"
-  docker push "${IMAGE}:latest"
-}
-
-function deployNext () {
-  ARTIFACT_IMAGE=$1
-  IMAGE=$2
-
-  echo "Deploying IMAGE=$IMAGE next"
-  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}:next"
-  docker push "${IMAGE}:next"
-}
-
-function deployOnbuild () {
-  ARTIFACT_IMAGE=$1
-  TAG=$2
-
-  docker build -t "wechaty/onbuild:$TAG" - <<__DOCKERFILE_ONBUILD__
-FROM $ARTIFACT_IMAGE
-
-ONBUILD ARG NODE_ENV
-ONBUILD ENV NODE_ENV \$NODE_ENV
-
-ONBUILD WORKDIR /bot
-
-ONBUILD COPY package.json .
-ONBUILD RUN jq 'del(.dependencies.wechaty)' package.json | sponge package.json \
-    && npm install \
-    && sudo rm -fr /tmp/* ~/.npm
-ONBUILD COPY . .
-
-ONBUILD RUN npm run build --if-present
-CMD [ "npm", "start" ]
-__DOCKERFILE_ONBUILD__
-
-  echo "Deploying wechaty/onbuild:$TAG"
-  docker push "wechaty/onbuild:$TAG"
-}
-
-function main () {
-  artifactImage='wechaty:artifact'
-  dockerImage='wechaty/wechaty'
+function dockerBuild () {
+  platform=$1     # `all` for all platforms, `linux/amd64`, `linux/arm64`, etc.
+  tag=${2}        # set the tag for building image
+  deploy=$3       # `true` for deploy, not set for local build
 
   # Shellcheck - https://github.com/koalaman/shellcheck/wiki/SC2086
   options=('--rm')
   [ -n "$NO_CACHE" ] && options+=('--no-cache')
 
-  declare -i ret=0
+  [ "$platform" == "all" ]  && platform='linux/amd64,linux/arm64,linux/arm/v7'
+  [ ! -z "$deploy" ]        && options+=('--push')
+
+  echo <<_CMD_
+    docker buildx build \
+    "${options[@]}" \
+    --platform "$platform" \
+    --tag "wechaty/wechaty:$tag" \
+    .
+_CMD_
+
+  docker buildx build \
+    "${options[@]}" \
+    --platform "$platform" \
+    --tag "wechaty/wechaty:$tag" \
+    .
+}
+
+function deployOnbuild () {
+  export tag=$1
+
+  echo "Building & Deploying 'wechaty/onbuild:$tag' based on 'wechaty/wechaty:build'"
+
+  cat Dockerfile.onbuild \
+    | sed "s#FROM wechaty/wechaty:next#FROM: wechaty/wechaty:build #" \
+    | docker xbuild build \
+      --platform 'linux/amd64,linux/arm64,linux/arm/v7' \
+      --tag "wechaty/onbuild:$tag" \
+      --push
+      -
+}
+
+function main () {
+declare -i ret=0
 
   case "$1" in
     build | '')
-      echo docker build "${options[@]}" -t "$artifactImage" .
-      exec docker build "${options[@]}" -t "$artifactImage" .
-
-  #    echo docker build "${options[@]}" -t "${artifactImage}:onbuild" -f Dockerfile.onbuild .
-  #    exec docker build "${options[@]}" -t "${artifactImage}:onbuild" -f Dockerfile.onbuild .
+      dockerBuild 'linux/amd64' build
 
       ret=$?
       ;;
@@ -80,12 +58,12 @@ function main () {
     test)
       echo "Testing the docker image behaviors to make sure it works as expected..."
       echo "bats tests/"
-      IMAGE_NAME="$artifactImage" bats tests/
+      IMAGE_NAME="$ARTIFACT_IMAGE" bats tests/
 
       echo
       echo
-      echo docker run -i "${options[@]}" -v /dev/shm:/dev/shm "$artifactImage" test
-      exec docker run -i "${options[@]}" -v /dev/shm:/dev/shm "$artifactImage" test
+      echo docker run -i --rm -v /dev/shm:/dev/shm "$ARTIFACT_IMAGE" test
+      exec docker run -i --rm -v /dev/shm:/dev/shm "$ARTIFACT_IMAGE" test
       ret=$?
       ;;
 
@@ -93,17 +71,17 @@ function main () {
       version=$(npx pkg-jq -r .version)
       majorVer=$(echo "$version" | cut -d. -f1)
       minorVer=$(echo "$version" | cut -d. -f2)
-      tag="${majorVer}.${minorVer}"
+      versionTag="${majorVer}.${minorVer}"
 
-      deployVersion "$artifactImage" "$dockerImage" "$tag"
-      deployOnbuild "$artifactImage" "$tag"
+      dockerBuild all "$versionTag" deploy
+      deployOnbuild "$versionTag"
 
       if npx --package @chatie/semver semver-is-prod "$version"; then
-        deployLatest "$artifactImage" "$dockerImage"
-        deployOnbuild "$artifactImage" latest
+        dockerBuild all latest deploy
+        deployOnbuild latest
       else
-        deployNext "$artifactImage" "$dockerImage"
-        deployOnbuild "$artifactImage" next
+        dockerBuild all next deploy
+        deployOnbuild next
       fi
       ;;
 
@@ -113,8 +91,8 @@ function main () {
       ;;
 
     *)
-      echo docker run -ti "${options[@]}" -v /dev/shm:/dev/shm "$artifactImage" "$@"
-      exec docker run -ti "${options[@]}" -v /dev/shm:/dev/shm "$artifactImage" "$@"
+      echo docker run -ti --rm -v /dev/shm:/dev/shm "$ARTIFACT_IMAGE" "$@"
+      exec docker run -ti --rm -v /dev/shm:/dev/shm "$ARTIFACT_IMAGE" "$@"
       ;;
   esac
 
