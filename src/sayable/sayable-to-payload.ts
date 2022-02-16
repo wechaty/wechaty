d76eddb0f8ab4be8d3fec8eb89e4474ca67a37ff
Huan (李卓桓) <zixia@zixia.net>
2022-02-16 09:41:57 +0800

    fix linting

--- src/sayable/sayable-to-payload.ts
+++ src/sayable/sayable-to-payload.ts
@@ -25,17 +25,17 @@ async function sayableToPayloads (sayable: Sayable): Promise<PUPPET.payloads.Say
   log.verbose('Wechaty', 'sayableToPayloads(%s)', sayable)
 
   if (typeof sayable === 'string') {
-    return [ PUPPET.payloads.sayable.text(sayable) ]
+    return [PUPPET.payloads.sayable.text(sayable)]
   } else if (typeof sayable === 'number') {
-    return [ PUPPET.payloads.sayable.text(String(sayable)) ]
+    return [PUPPET.payloads.sayable.text(String(sayable))]
   } else if (ContactImpl.valid(sayable)) {
-    return [ PUPPET.payloads.sayable.contact(sayable.id) ]
+    return [PUPPET.payloads.sayable.contact(sayable.id)]
   } else if (DelayImpl.validInstance(sayable)) {
     return []
   } else if (FileBox.valid(sayable)) {
-    return [ PUPPET.payloads.sayable.attatchment(sayable) ]
+    return [PUPPET.payloads.sayable.attatchment(sayable)]
   } else if (LocationImpl.validInstance(sayable)) {
-    return [ PUPPET.payloads.sayable.location(sayable.payload) ]
+    return [PUPPET.payloads.sayable.location(sayable.payload)]
   } else if (MessageImpl.valid(sayable)) {
     const messageSayable = await sayable.toSayable()
     if (!messageSayable) {
@@ -43,11 +43,11 @@ async function sayableToPayloads (sayable: Sayable): Promise<PUPPET.payloads.Say
     }
     return sayableToPayload(messageSayable)
   } else if (MiniProgramImpl.validInstance(sayable)) {
-    return [ PUPPET.payloads.sayable.miniProgram(sayable.payload) ]
+    return [PUPPET.payloads.sayable.miniProgram(sayable.payload)]
   } else if (PostImpl.validInstance(sayable)) {
-    return [ PUPPET.payloads.sayable.post(sayable.payload) ]
+    return [PUPPET.payloads.sayable.post(sayable.payload)]
   } else if (UrlLinkImpl.validInstance(sayable)) {
-    return [ PUPPET.payloads.sayable.url(sayable.payload) ]
+    return [PUPPET.payloads.sayable.url(sayable.payload)]
   } else {
     log.error('Wechaty', 'sayableToPayload() unsupported sayable: %s', sayable)
     return []
